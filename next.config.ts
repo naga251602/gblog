2d9d09392bf93b4be81f31827ce5cb4abfb83cda
naga251602 <rdy.other@protonmail.com>
2026-02-22 00:20:04 -0800

    chore: initialize next.js 15 project with tailwind v4 and supabase

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+// next.config.ts
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
